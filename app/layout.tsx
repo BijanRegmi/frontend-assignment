66790704ba2cc7e6a89436a00793dd77ec24e4e5
(void *)0x0 <55084653+BijanRegmi@users.noreply.github.com>
2023-05-28 11:13:31 +0545

    Formatting to get started with

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,17 @@
-import "./globals.css";
-import { Inter } from "next/font/google";
+import "./globals.css"
+import { Inter } from "next/font/google"
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] })
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+    console.log("Test")
+    return (
+        <html lang="en">
+            <body className={inter.className}>{children}</body>
+        </html>
+    )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <p>Good luck !</p>;
+    return <p>Good luck !</p>
 }
