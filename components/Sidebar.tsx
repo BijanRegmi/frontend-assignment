a8cc699cce412d3b1c113f8a1a1f99d23598ec25
(void *)0x0 <55084653+BijanRegmi@users.noreply.github.com>
2023-05-28 19:08:15 +0545

    Dummy sidebar

---
+++ components/Sidebar.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link"
+import { AiOutlineSearch } from "react-icons/ai"
+import { MdOutlineEvent } from "react-icons/md"
+import { FiRadio } from "react-icons/fi"
+import { BiTrendingUp } from "react-icons/bi"
+import { SlEvent } from "react-icons/sl"
+
+export const Sidebar = () => {
+    const links = [
+        { title: "Home", icon: MdOutlineEvent, href: "" },
+        { title: "Browse", icon: AiOutlineSearch, href: "" },
+        { title: "Radio", icon: FiRadio, href: "" },
+        { title: "Trending", icon: BiTrendingUp, href: "" },
+        { title: "Events", icon: SlEvent, href: "" },
+    ]
+
+    return (
+        <div className="w-52 h-full overflow-scroll">
+            <ul className="flex flex-col justify-around gap-4 bg-shade-0 text-shade-900 rounded-md py-8">
+                {links.map((link, idx) => (
+                    <li
+                        key={idx}
+                        className="flex gap-4 items-center cursor-pointer px-2"
+                    >
+                        <link.icon className="h-10 w-10 p-2" />
+                        <Link href={link.href} className="grow font-semibold">
+                            {link.title}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
